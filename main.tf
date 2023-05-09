aa5fc29afa762feabb28abba344a8b8419efaa77
rmharley66 <rmharley66@gmail.com>
2023-05-09 15:53:26 -0400

    Adding a better security group

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
+
 // Create an EC2 instance
 /*
   - Name your resources appropriately and use descriptive names
@@ -26,13 +27,12 @@ resource "aws_instance" "rico-ec2-deploy" {
   ami           = "ami-06d5c50c30a35fb88"
   instance_type = "t2.micro"
   key_name      = "rico-ec2kp"
-  security_groups = ["rico-spring-boot"]
+  security_groups = ["rico-ec2-sc"]
   tags = {
     Name = "rico-ec2-deploy" 
   }
+  
 }
-
-
 
 
 // Create a security group for the EC2 instance
@@ -40,3 +40,27 @@ resource "aws_instance" "rico-ec2-deploy" {
   - Name your resources appropriately and use descriptive names
   - Use a Security Group that allows SSH access from anywhere
 */
+
+
+resource "aws_security_group" "rico-ec2-sc" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
