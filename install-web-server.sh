e208a8353c1125b16d927fe901a709a45f544fd2
rmharley66 <rmharley66@gmail.com>
2023-05-10 13:03:01 -0400

    Redployed based on region

--- install-web-server.sh
+++
@@ -1,6 +0,0 @@
-sudo su -c 'echo "#!/bin/bash
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo \"<h1>Hello World from \$(hostname -f)</h1>\" > /var/www/html/index.html" > /install-web-server.sh'
