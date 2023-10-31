dfb7a006111eadf9304980f2ddbcdd0840eb529f
ColbyDowns <colbyjdowns@outlook.com>
2023-10-31 16:26:28 -0500

    adding user data script

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ resource "aws_instance" "web" {
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [var.security_group_name]
   key_name                = "cjd-tf-keypair"
+  user_data               = file("install-web-server.sh")
 
   tags = {
     Name = "cjd-tf-ec2"
@@ -33,32 +34,3 @@ resource "aws_instance" "web" {
 }
 
 
-// Create a security group for the EC2 instance
-/*
-  - Name your resources appropriately and use descriptive names
-  - Use a Security Group that allows SSH access from anywhere
-*/
-resource "aws_security_group" "allow_ssh" {
-  name        = var.security_group_name
-  description = "Allow SSH inbound traffic"
-  vpc_id      = var.default_vpc
-
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = var.ssh_port
-    to_port          = var.ssh_port
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "cjd-allow-ssh"
-  }
-}
